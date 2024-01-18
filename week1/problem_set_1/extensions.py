31f97e29490adf93568966da85b748037a96d785
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-01-18 20:50:22 +0530

    extension problem

---
+++ week1/problem_set_1/extensions.py
@@ -0,0 +1,18 @@
+#Program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends with extension type
+
+file = input("File name: ")
+
+if file.endswith(".gif"):
+    print("image/gif")
+elif file.endswith(".jpg") or file.endswith(".jpeg"):
+    print("image/jpeg")
+elif file.endswith(".png"):
+    print("image/png")
+elif file.endswith(".pdf") or file.strip().endswith(".PDF"):
+    print("application/pdf")
+elif file.endswith(".txt"):
+    print("text/plain")
+elif file.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
