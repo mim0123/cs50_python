4788e2651479b4cfe394ea974d8fe6dc8c278b72
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-01-30 12:30:35 +0530

    sys.argv

---
+++ week4/name.py
@@ -0,0 +1,28 @@
+# Command-line Arguments
+# Study documentation for sys
+# use of sys.argv (argument vector)
+
+import sys
+
+print("hello, my name is", sys.argv[1]) 
+
+
+# what is in sys.argv[0]? - the name of the program i.e; name.py
+
+# Fix the sys.arg[1] problem
+import sys 
+try: 
+    print("hello, my name is", sys.argv[1])
+except IndexError:
+    print("Too few arguments")
+
+
+
+# Another way
+import sys
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("Hello my name is", sys.argv[1])
