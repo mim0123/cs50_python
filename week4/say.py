c8134ee55b4acfa910ee94357e9123053bb75adc
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-01-31 10:46:16 +0530

    use of pep8

--- week4/say.py
+++ week4/say.py
@@ -1,6 +1,5 @@
-import cowsay
 import sys
 
-if len (sys.argv) == 2:
-    cowsay.trex("hello, " + sys.argv[1]) # THis will print out a cow
-    cowsay.cow("hello, " + sys.argv[1])  # This will print out a trex
+from sayings import hello
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
