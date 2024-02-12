084adfd72be09de30955240a38cdf7419eebc305
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-02-12 12:00:40 +0530

    return value test

--- week5/hello.py
+++ week5/hello.py
@@ -1,9 +1,9 @@
 def main():
     name = input ("what is your name? ")
-    hello (name)
+    print(hello (name))
 
 def hello(to="world"):
-    print("hello,", to)
+    return f"hello, {to}"
 
 
 if __name__ == "__main__":
--- week5/test_hello.py
+++ week5/test_hello.py
@@ -1,4 +1,8 @@
 from hello import hello 
 
-def test_hello():
+def test_default():
     hello("David") == "hello, David"
+
+
+def test_argument():
+    assert hello() == "hello, world"
