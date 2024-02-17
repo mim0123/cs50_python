b738ad7f3b77e05258d6339c8b9cfaee6abcb4c6
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-02-17 11:54:31 +0530

    DictWriter

--- week6/students.py
+++ week6/students.py
@@ -1,18 +1,8 @@
-students = []
-
 import csv 
 
-students = []
+name = input("what is your name? ")
+home = input("where is your house? ")
 
-
-with open ("students.csv") as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        students.append({"name": row["name"], "home": row["home"]})
-
-
-    
-
-for student in sorted (students, key = lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
-      
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name":name, "home": home})
