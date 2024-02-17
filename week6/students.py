9ad0ed790d45a9306f2c49f09b6862f3084a5413
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-02-17 10:31:04 +0530

    sorting the table using sort key

--- week6/students.py
+++ week6/students.py
@@ -1,5 +1,17 @@
-with open("students.csv") as file:
+students = []
+
+with open ("students.csv") as file:
     for line in file:
-        name, house = line.rstrip(). split(",")
-        print(f"{name} is in {house}")
-        
+        name, house = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+
+
+for student in sorted (students, key = get_name):
+    print(f"{student['name']} is in {student['house']}")
+      
