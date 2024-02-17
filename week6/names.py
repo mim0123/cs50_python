00070730d778acd0c846cfe0455a90efe67457a1
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-02-17 10:22:26 +0530

    make a csv

--- week6/names.py
+++ week6/names.py
@@ -1,6 +1,10 @@
-with open ("names.txt", "r") as file:
-    lines = file.readlines()
+names = []
+
+with open ("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
 
 
-for line in lines:
-    print("hello,", line)
+for name in sorted (names, reverse=True):
+    print(f"hello, {name}")
+
---
+++ week6/students.py
@@ -0,0 +1,5 @@
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip(). split(",")
+        print(f"{name} is in {house}")
+        
