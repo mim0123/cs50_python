00cc3b60c4b0a59b173f8bfe314701d108c83eaf
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-02-22 21:41:45 +0530

    use of ^ and $

--- week7/validate.py
+++ week7/validate.py
@@ -1,7 +1,7 @@
 import re
 email = input("what's your email? ").strip()
 
-if re.search(r".+@.+\.edu", email):
+if re.search(r"^.+@.+\.edu$", email):
     print("valid")
 else:
     print("Invalid")
