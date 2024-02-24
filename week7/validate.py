8585427049ec3d1568ea0367ca379d58fd7c8117
mim0123 <155512239+mim0123@users.noreply.github.com>
2024-02-24 12:19:49 +0530

    Grouping

--- week7/validate.py
+++ week7/validate.py
@@ -1,7 +1,7 @@
 import re
 email = input("what's your email? ").strip()
 
-if re.search(r"^\w+@\w+\.edu$", email):
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("valid")
 else:
     print("Invalid")
